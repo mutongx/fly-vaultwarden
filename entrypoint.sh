f57885c5be0024caa96b6b018d73fb4ec76efda2
Mut Ze <mutongsoft@gmail.com>
2023-06-05 16:59:45 +0800

    Store rclone config

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 
 test -n "$CLOUDFLARED_SERVICE_TOKEN" || (echo '$CLOUDFLARED_SERVICE_TOKEN is not set' && exit 1)
 
+mkdir -p /root/.config/rclone
+echo "$RCLONE_CONFIG_CONTENT" >/root/.config/rclone/rclone.conf
+
 cloudflared service install "$CLOUDFLARED_SERVICE_TOKEN"
 
 cd /root/vaultwarden
