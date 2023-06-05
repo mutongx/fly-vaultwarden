cee852e5c71f5f7dd2d0af107547e5ed463911d8
Mut Ze <mutongsoft@gmail.com>
2023-06-05 17:27:46 +0800

    Add set -x

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 BACKUP_ID="$(date +%Y-%m-%d-%H-%M-%S-%s)"
 for REMOTE in ${RCLONE_BACKUP_REMOTES//,/ }; do
     /root/rclone/rclone copy /root/vaultwarden/data "$REMOTE/$BACKUP_ID"
